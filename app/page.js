a9457112aa87578554e9592232be711579e95628
asajjad2 <areebsajjad21@gmail.com>
2023-11-01 19:54:25 +0500

    shopify setup

--- app/page.js
+++ app/page.js
@@ -1,6 +1,12 @@
 import Image from 'next/image'
+import { shopifyClient, parseShopifyResponse } from '../lib/shopify'
+export default async function Home() {
 
-export default function Home() {
+  // fetch all products
+
+  const products = await shopifyClient.product.fetchAll()
+  console.log(parseShopifyResponse(products))
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
